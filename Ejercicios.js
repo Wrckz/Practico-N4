7e4d9550c1859ccc3c6b68ec5f54c954ebb9128a
Wrckz <109772318+Wrckz@users.noreply.github.com>
2022-07-25 21:06:11 -0300

    Add files via upload

--- Ejercicios.js
+++ Ejercicios.js
@@ -1,48 +1,77 @@
-function Obtener(obj) {
-    //Obtener y Recorrer las Llaves de un Objeto
-    //Aqui tu codigo
-    conso
-
-}
-
+function Obtener(Object) {
+    var persona = {
+      edad: 35,
+      nombre: 'Bruno',
+      apellido: 'Perez',
+      email: 'bruno@mail.com',
+    };
+    for (let propiedad in persona) {
+      console.log(propiedad);
+    }
+  }
+  
 function Fecha(num) {
-    //Escriba una función de JavaScript para obtener el día de una fecha en particular.
-    //Aqui tu codigo
+    var date = new Date();
+    console.log(date);
 
 }
 
 function verificarPassword(usuario, password) {
-    // Comprueba si la "password" enviada coincide con la propiedad "password" del objeto "usuario"
-    // Devuelve "true" si coinciden
-    // De lo contrario, devuelve "false"
-    // // Tu código:
-    console.log("Hola")
-    var a=40
-
+    var usuario = {
+        nombre,
+        email,
+        password
+    }
+    if (usuario.password !== undefined){
+        return true
+    } else {
+        return false
+    }
 }
+
 
 function crearGato(nombre, edad) {
-    // Crear un nuevo objeto con la propiedad "nombre" y el valor definido como el argumento "nombre".
-    // Agrega una propiedad al objeto con el nombre "edad" y usa el valor definido en el argumento "edad"
-    // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
-    // Devuelve el objeto
-    // Tu código:
+    var objeto {
+        nombre: Cato
+
+    }
+    function agregarPropiedad(objeto,property) {
+
+        objeto[property] = null
+    
+        return objeto
+    
+    }
+    
+    console.log(agregarPropiedad({},"edad"))
+
+    console.log(objeto)
+
 
 }
+
+
 function agregarMetodoCalculoDescuento(producto) {
-    // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
-    // Este método debe multiplicar el "precio" del "producto" ("producto.precio" o "producto[precio]") y "porcentajeDeDescuento" para obtener el descuento
-    // El método resta el descuento del precio y devuelve el precio con descuento
-    // Devuelve el objeto "producto" al final de la función
-    // Ejemplo:
-    // producto.precio -> 20
-    // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
-    // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
-    // Tu código:
 
-}
+    producto.calcularPrecioDescuento = function(){
+        let descuento;
+        descuento = this.precio * this.porcentajeDeDescuento;
+        return this.precio - descuento;
+      }
+      return producto;
+    }console.log(agregarMetodoCalculoDescuento({"precio": 20, "porcentajeDeDescuento": .1}))
+
+
 function ordenar(array) {
-    // A partir del array pasado por parametros, ordenarlo (ascendente o descendente) de la forma que tu quieras
-    // Mostrar array ordenado.
-    // Tu código:
+  let numbers = [-7, -5, -3, 2, 73, 24, 60, 89, 6, 44, 1];
+numbers.sort((a, b) => {
+  if(a == b) {
+    return 0; 
+  }
+  if(a < b) {
+    return -1;
+  }
+  return 1;
+});
 }
+console.log(numbers)
